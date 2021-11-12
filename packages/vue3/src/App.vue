dae9dbdfe257f9ccb6d11cd82d87cf56473de2ee
lixu <461229187@qq.com>
2021-11-12 17:59:34 +0800

    fix: 🐛 props 测试

--- packages/vue3/src/App.vue
+++ packages/vue3/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div>{{ grind }}</div>
   <p>{{ props.name }}</p>
+  <p>{{ props.labels }}</p>
 </template>
 
 <script lang="ts" setup>
 import { ref, defineProps } from "vue";
 import { core } from '@x-grind/core';
 
-const props = defineProps<{
-  name: string
-}>()
+interface Props {
+  msg?: string
+  labels?: string[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  name: 'lx',
+  labels: () => ['one', 'two']
+})
 
 const grind = ref(core());
 </script>
